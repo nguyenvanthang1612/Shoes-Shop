5ba9a3d19670e78c5f45b9a7f3f79f154d6dc496
camnh <nguyenhuucam91@gmail.com>
2021-05-27 09:00:38 +0700

    Update Auth

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,11 +3,7 @@
 namespace App\Providers;
 
 use App\Models\Category;
-use App\Models\Product;
 use App\Models\User;
-use GuzzleHttp\Psr7\Request;
-use Illuminate\Contracts\Pagination\Paginator;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
@@ -30,14 +26,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-<<<<<<< HEAD
-        View::composer(['frontend.layouts._nav','frontend.index','frontend.product.shop-item','frontend.product.shop-list'], function($view) {
-            return $view->with('categories', Category::all());
-        });
-
-        $users = User::all();
-        View::share('', compact('users'));
-=======
         if (!app()->runningInConsole()) {
             View::composer(['frontend.layouts._nav','frontend.index','frontend.product.shop-item','frontend.product.shop-list'], function($view) {
                 return $view->with('categories', Category::all());
@@ -47,6 +35,5 @@ class AppServiceProvider extends ServiceProvider
             View::share('', compact('users'));
         }
 
->>>>>>> b7042c6f6dbd3d18903b7fc9056127eff9ae1fc3
     }
 }
