82d1c839481af6b0134ba92d0689b8f152b40fdc
nguyenvanthang1612 <hungdongho1666@gmail.com>
2021-06-06 17:16:03 +0700

    change 6/6/2021

--- resources/views/frontend/product/show.blade.php
+++ resources/views/frontend/product/show.blade.php
@@ -46,6 +46,8 @@
                                                 </div>
                                             @endforeach
                                         </div>
+
+
                                     </div>
                                 </div><!-- / ITEM GALLERY BLOCK -->
 
