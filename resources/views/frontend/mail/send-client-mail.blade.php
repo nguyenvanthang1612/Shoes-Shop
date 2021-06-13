74b714529e020570f995448d2563e84095d91378
nguyenvanthang1612 <hungdongho1666@gmail.com>
2021-06-13 13:54:14 +0700

    change

---
+++ resources/views/frontend/mail/send-client-mail.blade.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="keywords" content="">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <link rel="icon" href="assets/images/favicon.png" type="image/png">
+  <title>Order Success</title>
+    
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+          <script src="js/html5shiv.min.js"></script>
+          <script src="js/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body style="background: #e4e9f0;">
+	<div style="margin:0 auto;max-width:600px;">
+    
+    <table width="100%"  border="0" style="border-collapse:collapse; font-family:Arial, Helvetica, sans-serif;margin-top: 20px;
+     font-size:14px; line-height:22px;">
+        <tr>
+            <td style="padding:20px; background:#FFF;">
+                <table width="100%" border="0" cellpadding="7">
+                    <tr>
+                        <td>Hi, {{ $email }}</td>
+                    </tr>
+                    <tr>
+                        <td>
+                            We have confirmed your order, thanks for trusting our store
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Thank you<br>
+                    Q and T shoes shop</td>
+                    </tr>
+                </table>
+            </td>
+        </tr>
+    </table>
+  
+	</div>
+	
+
+</body>
+
+</html>
---
+++ resources/views/frontend/mail/send-subcribe-mail.blade.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="keywords" content="">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <link rel="icon" href="assets/images/favicon.png" type="image/png">
+  <title>Subcribe Mail</title>
+    
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+          <script src="js/html5shiv.min.js"></script>
+          <script src="js/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body style="background: #e4e9f0;">
+	<div style="margin:0 auto;max-width:600px;">
+    
+    <table width="100%"  border="0" style="border-collapse:collapse; font-family:Arial, Helvetica, sans-serif;margin-top: 20px;
+     font-size:14px; line-height:22px;">
+        <tr>
+            <td style="padding:20px; background:#FFF;">
+                <table width="100%" border="0" cellpadding="7">
+                    <tr>
+                        <td>Hi, {{ $email }}</td>
+                    </tr>
+                    <tr>
+                        <td>Thank for your subcribed {{ $name }}</td>
+                    </tr>
+                    <tr>
+                        <td>
+                            If you have already registered, please visit our website immediately to update more new items
+                        </td>
+                    </tr>
+                    <tr>
+                        <td><a href="{{ route('frontend.index') }}" style="display:inline-block;text-decoration:none;background:#00a8ff;border:1px solid #00a8ff;border-radius:2px;color:#FFF;font-size:14px;font-weight:400;padding:13px 22px;border-radius: 25px;" target="_blank">
+                            Click This Link 
+                        </a></td>
+                    </tr>
+                    <tr>
+                        <td>Thank you<br>
+                    Q and T shoes shop</td>
+                    </tr>
+                </table>
+            </td>
+        </tr>
+    </table>
+  
+	</div>
+	
+
+</body>
+
+</html>
