6254653d4207bfba443c2dfaa7522e97fc7a308d
nguyenvanthang1612 <hungdongho1666@gmail.com>
2021-09-17 14:51:52 +0700

    a

--- database/migrations/2021_09_17_143747_create_blogs_table.php
+++ database/migrations/2021_09_17_143747_create_blogs_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddImage extends Migration
+class CreateBlogsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,8 +13,14 @@ class AddImage extends Migration
      */
     public function up()
     {
-        Schema::table('blogs', function (Blueprint $table) {
+        Schema::create('blogs', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->tinyText('active');
+            $table->string('title');
+            $table->longText('content');
             $table->string('image');
+            $table->timestamps();
         });
     }
 
@@ -25,8 +31,6 @@ class AddImage extends Migration
      */
     public function down()
     {
-        Schema::table('blogs', function (Blueprint $table) {
-            //
-        });
+        Schema::dropIfExists('blogs');
     }
 }
